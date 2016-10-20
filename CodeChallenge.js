ff0a075d8cc6d1eff3c0a0e5ad8ce1eb753cda48
Donovan Hiland <donovan.hiland@gmail.com>
2016-10-19 18:43:26 -0600

    code challenge

---
+++ CodeChallenge.js
@@ -0,0 +1,57 @@
+/*********Challenges*********/
+// Hair Color
+// Create a variable called hairColor and set it equal to a string of your hair color.
+
+
+
+// whichChild
+// Create a function called whichChild that takes one parameter: a child's name. // When passed in ‘Dave’ return ‘Oldest’
+// When passed in ‘Michelle’ return ‘Middle’
+// When given any other value return ‘Not my kid!”
+
+
+
+// Sum
+// Create a function called sum that returns the sum of two numbers passed in as parameters.
+
+
+
+// Triple
+// Create a function called triple that takes in a number as a parameter, triples the number, and returns the result.
+
+
+
+// numbaOne
+// Create a function called numbaOne that returns the first element of an array that is passed in as a parameter. Hint: This should work for an array of any size.
+
+
+
+// theLastOne
+// Create a function called theLastOne that returns the last element of an array that is passed in. Hint: This should work for an array of any size.
+
+
+
+// addThemUp
+// Create a function called addThemUp that takes in an array of numbers and returns the total of all of the items in the array added together.
+
+
+
+// loveTheThrees
+// Write a function called loveTheThrees that takes in an array full of numbers. It should loop through the array and adds any numbers that are divisible by 3 to a new array called threes. Once you have added all the numbers that are divisible by 3 to the new array, then return the threes array.
+
+
+
+// Hobbit
+// Create an object called "hobbit". Give it a key called "name" with a value of "Gollum" and another key called "my" with a value of "precious".
+
+
+
+// superSecret
+// In the "superSecret" function “spy” will be a passed in as an object that represents a spy. Loop through all the properties on the spy object and change all values to the string “redacted”, then return the updated spy object.
+
+function superSecret(spy){
+// Code Here
+}
+
+// onlyTruthy
+// Make a function called onlyTruthy that takes in an object, loops through all its properties, and removes any that are falsy. Then return the object that was passed in. (hint: delete)
